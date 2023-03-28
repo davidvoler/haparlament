2ebb69e45f180935c55e0c94a8a57d9f2b1a678d
david <davidvoler@gmail.com>
2023-03-28 17:58:41 +0300

    using original ui code

--- frontend/ui/src/App.js
+++ frontend/ui/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PresetFeed from "./components/presetFeed/PresetFeed";
+import WaitingForPairing from "./waitingForPairing/WaitingForPairing";
 
 function App() {
+  const user = null;
+  const [waiting, setWaiting] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <PresetFeed setWaiting={setWaiting} />
+      {waiting && <WaitingForPairing />}
     </div>
   );
 }
---
+++ frontend/ui/src/waitingForPairing/WaitingForPairing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function WaitingForPairing() {
+  return (
+    <div>
+      <h1>Waiting for pairing</h1>
+    </div>
+  );
+}
+
+export default WaitingForPairing;
